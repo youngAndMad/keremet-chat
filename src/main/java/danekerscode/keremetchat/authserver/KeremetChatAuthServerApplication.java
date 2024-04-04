40063db9170d47bd904b0cef19540758d59d3011
Daneker Kaliaskaruly <kaliaskaruly@ezdrav.kz>
2024-04-04 22:42:27 +0500

    init

---
+++ src/main/java/danekerscode/keremetchat/authserver/KeremetChatAuthServerApplication.java
@@ -0,0 +1,13 @@
+package danekerscode.keremetchat.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeremetChatAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeremetChatAuthServerApplication.class, args);
+	}
+
+}
