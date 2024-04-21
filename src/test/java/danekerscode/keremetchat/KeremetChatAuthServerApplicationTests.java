b8332cf75f27325aa0a7a9cdbc7e89d907a58d75
youngAndMad <kkraken2005@gmail.com>
2024-04-21 22:54:33 +0500

    no @SpringBootTest

--- src/test/java/danekerscode/keremetchat/KeremetChatAuthServerApplicationTests.java
+++ src/test/java/danekerscode/keremetchat/KeremetChatAuthServerApplicationTests.java
@@ -1,12 +1,7 @@
 package danekerscode.keremetchat;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class KeremetChatAuthServerApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
