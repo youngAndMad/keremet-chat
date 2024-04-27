1533eb78613f87afc7b47c0f93fbf9cfd87da841
youngandmad <kkraken2005@gmail.com>
2024-04-27 18:46:12 +0600

    file utils test

--- src/main/java/danekerscode/keremetchat/utils/FileUtils.java
+++ src/main/java/danekerscode/keremetchat/utils/FileUtils.java
@@ -3,17 +3,25 @@ package danekerscode.keremetchat.utils;
 import danekerscode.keremetchat.common.AppConstants;
 import danekerscode.keremetchat.model.enums.FileEntitySource;
 import lombok.experimental.UtilityClass;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 import org.springframework.web.multipart.MultipartFile;
 
 @UtilityClass
 public class FileUtils {
 
     public static String getMinioObjectPath(
-            String bucketName,
-            MultipartFile file,
-            FileEntitySource source,
-            String target
+            @NonNull String bucketName,
+            @NonNull MultipartFile file,
+            @NonNull FileEntitySource source,
+            @NonNull String target
     ) {
+        Assert.hasLength(bucketName,"Not empty bucket name is required");
+        Assert.hasLength(target,"Not empty target name is required");
+        Assert.notNull(source, "Not null file source is required");
+        Assert.notNull(file, "Not null file is required");
+
+
         return bucketName
                 .concat("/")
                 .concat(source.getPath())
@@ -23,9 +31,10 @@ public class FileUtils {
                 .concat(file.getName());
     }
 
-    public static String getFileExtension(MultipartFile file) {
-        var dotIndex = file.getName().lastIndexOf(AppConstants.DOT.getValue());
+    public static String getFileExtension(@NonNull MultipartFile file) {
+        Assert.notNull(file, "Not null file is required");
 
-        return dotIndex == -1 ? "" : file.getName().substring(dotIndex);
+        var dotIndex = file.getName().lastIndexOf(AppConstants.DOT.getValue());
+        return dotIndex == -1 ? AppConstants.EMPTY_STRING.getValue() : file.getName().substring(dotIndex);
     }
 }
---
+++ src/test/java/danekerscode/keremetchat/utils/FileUtilsTest.java
@@ -0,0 +1,61 @@
+package danekerscode.keremetchat.utils;
+
+import danekerscode.keremetchat.common.AppConstants;
+import danekerscode.keremetchat.model.enums.FileEntitySource;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilsTest {
+
+    @Test
+    void getFileExtension_FileWithExtension_ReturnsExtension() {
+        var file = new MockMultipartFile("some.txt", new byte[0]);
+
+        var fileExtension = FileUtils.getFileExtension(file);
+
+        assertEquals(fileExtension, "txt");
+    }
+
+    @Test
+    void getFileExtension_FileWithoutExtension_ReturnsEmptyString() {
+        var file = new MockMultipartFile("some_file_without_extension", new byte[0]);
+
+        var fileExtension = FileUtils.getFileExtension(file);
+
+        assertEquals(fileExtension, AppConstants.EMPTY_STRING.getValue());
+    }
+
+    @Test
+    void getMinioObjectPath_ValidInputs_ReturnsObjectPath(){
+        var file = new MockMultipartFile("some.png", new byte[0]);
+
+        var minioObjectPath = FileUtils.getMinioObjectPath(
+                AppConstants.MINIO_DEFAULT_BUCKET.getValue(),
+                file,
+                FileEntitySource.CHAT_AVATAR,
+                "1"
+        );
+
+        var expectedObjectPath = AppConstants.MINIO_DEFAULT_BUCKET.getValue()
+                .concat("/")
+                .concat(FileEntitySource.CHAT_AVATAR.getPath())
+                .concat("/")
+                .concat("1")
+                .concat("/")
+                .concat(file.getName());
+
+        assertEquals(expectedObjectPath,minioObjectPath);
+    }
+
+    @Test
+    void getMinioObjectPath_NullInput_ThrowsException(){
+        var file = (MockMultipartFile) null;
+
+        assertThrows(IllegalArgumentException.class, () -> FileUtils.getFileExtension(file));
+    }
+
+
+
+}
